7d07208c7dfbfc6fcf5b3a128db3564f7ffb69e2
Joyce van den Berg <37065272+daenelia@users.noreply.github.com>
2021-08-31 10:39:43 +0200

    Update script.js
    Added the activities cost, payment option and started the validation of fields.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //focus on name field at load
-document.getElementById("name").focus();
+const registerName = document.getElementById("name")
+registerName.focus();
 
 //hide other job role text field on load
 const hideRole = document.getElementById("other-job-role");
@@ -27,7 +28,7 @@ const pickColor = document.getElementById('design');
 const hidePuns = document.querySelectorAll('[data-theme="js puns"]');
 const hideHearts = document.querySelectorAll('[data-theme="heart js"]');
 
-pickColor.addEventListener('change', (event) => {
+pickColor.addEventListener("change", (event) => {
     if (event.target.value === 'js puns'){
         hideColor.style.display = "block";
         for (let i = 0; i < hideHearts.length; i++) {
@@ -46,4 +47,80 @@ pickColor.addEventListener('change', (event) => {
         hideColor.style.display = "none";
     }
 });
+
+//The "Total: $" element below the "Register for Activities" section updates to reflect the sum of the cost of the user’s selected activities.
+
+const activities = document.getElementById("activities");
+const activitiesTotal = document.getElementById("activities-cost");
+let totalCost = 0;
+
+activities.addEventListener("change", (event) => {
+    let cost = +event.target.getAttribute("data-cost");
+    
+    if(event.target.checked == true){
+        totalCost = totalCost + cost;
+        activitiesTotal.innerHTML = "$ " + (totalCost);
+    }
+    if(event.target.checked != true){
+        totalCost = totalCost - cost;
+        activitiesTotal.innerHTML = "Total: $ " + (totalCost);
+    }
+});
+
+// Payment info
+
+const payWith = document.getElementById("payment");
+const paypal = document.getElementById("paypal");
+const creditCard = document.getElementById("credit-card");
+const bitcoin = document.getElementById("bitcoin");
+
+paypal.style.display = "none";
+bitcoin.style.display = "none";
+payWith.value = "credit-card"; 
+
+payWith.addEventListener("change", (event) => {
+    if(event.target.value === 'paypal'){
+        console.log("Ow! Yeah!");
+        creditCard.style.display = "none";
+        bitcoin.style.display = "none";
+        paypal.style.display = "block";
+        
+    }
+    else if(event.target.value === 'bitcoin'){
+        console.log("Ow! OW! Yeah!");
+        creditCard.style.display = "none";
+        paypal.style.display = "none";
+        bitcoin.style.display = "block";
+    }
+    else {
+        paypal.style.display = "none";
+        bitcoin.style.display = "none";
+        creditCard.style.display = "block";
+    }
+});
+
+// Validation of fields - get the fields and the button
+const registerEmail = document.getElementById('email');
+const registerCard = document.getElementById('cc-num');
+const registerZip = document.getElementById('zip');
+const registerCVV = document.getElementById('cvv');
+// cant'quite catch the button for clicking...
+const button = document.getElementsByTagName('BUTTON')[0];
+const form = document.querySelector('form');
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Preventing the form from actually being submitted and refreshing the page
+    const inputName = registerName.value;
+    console.log(inputName);
+    const checkName = /^[a-zA-Z ]{2,30}$/;
+    if (inputName.match(checkName)){
+        console.log("It is a good name.");
+    }
+    else{
+        console.log("Is that a name?");
+    }
+});
+
+
 
